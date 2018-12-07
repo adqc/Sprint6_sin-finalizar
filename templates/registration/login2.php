0c3a1c56dae8f832ecfeedddcd2a6a9031762df3
Andres <andres.liu22@gmail.com>
2018-12-07 10:02:11 -0500

    no message

--- templates/registration/login2.php
+++ templates/registration/login2.php
@@ -45,33 +45,35 @@
     </div>
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script src="{% static 'designer/js/jquery.cookie.js' %}"></script>
+
   <script>
 
-    var person = { userID: "", first_name: "", last_name:"", accessToken: "", email: "";
-    var csrftoken = $.cookie('csrftoken');
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  function getCookie(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie != '') {
+          var cookies = document.cookie.split(';');
+          for (var i = 0; i < cookies.length; i++) {
+              var cookie = jQuery.trim(cookies[i]);
+              // Does this cookie string begin with the name we want?
+              if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                  cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                  break;
+              }
+          }
+      }
+      return cookieValue;
     }
 
     $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
+      beforeSend: function(xhr, settings) {
+          if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+              // Only send the token to relative URLs i.e. locally.
+              xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+          }
+      }
     });
+    var person = { userID: "", first_name: "", last_name:"", accessToken: "", email: "";
 
-    $.ajax(save_url, {
-        type : 'POST',
-        contentType : 'application/json',
-        data : JSON.stringify(canvas),
-        success: function () {
-            console.log("paso");
-        }
-
-    });
     function logIn(){
 
       FB.login(function (response){
