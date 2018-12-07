300e11001e75c13dd3d2231c5a114cd316cd0e0b
Andres <andres.liu22@gmail.com>
2018-12-07 10:50:43 -0500

    no message

--- templates/registration/login2.php
+++ templates/registration/login2.php
@@ -46,42 +46,10 @@
     </div>
 
   <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-  <script>
-  function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-        return cookieValue;
-    }
-    var csrftoken = getCookie('csrftoken');
-    console.log(csrftoken);
 
-    //Ajax call
-    function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-    $.ajaxSetup({
-        crossDomain: false, // obviates need for sameOrigin test
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type)) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-  </script>
 
   <script>
-    var person = { userID: "", first_name: "", last_name:"", accessToken: "", email: "", 'csrfmiddlewaretoken':'CSRF-TOKEN-VALUE'};
+    var person = { userID: "", first_name: "", last_name:"", accessToken: "", email: ""};
     function logIn(){
 
       FB.login(function (response){
@@ -94,6 +62,38 @@
                 person.first_name=userData.first_name;
                 person.last_name=userData.last_name;
                 person.email=userData.email;
+
+            function getCookie(name) {
+                  var cookieValue = null;
+                  if (document.cookie && document.cookie != '') {
+                      var cookies = document.cookie.split(';');
+                      for (var i = 0; i < cookies.length; i++) {
+                          var cookie = jQuery.trim(cookies[i]);
+                          // Does this cookie string begin with the name we want?
+                          if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                              break;
+                          }
+                      }
+                  }
+                      return cookieValue;
+                  }
+                  var csrftoken = getCookie('csrftoken');
+                  console.log(csrftoken);
+
+                  //Ajax call
+                  function csrfSafeMethod(method) {
+                  // these HTTP methods do not require CSRF protection
+                  return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+                  }
+                  $.ajaxSetup({
+                      crossDomain: false, // obviates need for sameOrigin test
+                      beforeSend: function(xhr, settings) {
+                          if (!csrfSafeMethod(settings.type)) {
+                              xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                          }
+                      }
+                  });
 
                 $.ajax({
                   type:"POST",
